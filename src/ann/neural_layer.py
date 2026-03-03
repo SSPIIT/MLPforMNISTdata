659db615bcbf3fce9f93e1401686fecdf7f372fd
unknown <swarnil2004@gmail.com>
2026-03-03 13:19:26 +0530

    Added linear layer

--- src/ann/neural_layer.py
+++ src/ann/neural_layer.py
@@ -2,3 +2,33 @@
 Neural Layer Implementation
 Handles weight initialization, forward pass, and gradient computation
 """
+
+import numpy as np
+
+class LinearLayer:
+    def __init__(self, input_dim, output_dim, weight_init="random"):
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+        if weight_init == "random":
+            self.W = np.random.randn(input_dim, output_dim)*0.01
+        elif weight_init == "xavier":
+            limit = np.sqrt(6 / (input_dim + output_dim))
+            self.W = np.random.uniform(-limit, limit, (input_dim,output_dim))
+        else: 
+            raise ValueError("Invalid weight initialization")
+        
+        self.b = np.zeros((1, output_dim))
+
+        self.grad_W = None
+        self.grad_b = None
+        self.X = None
+
+    def forward(self, X):
+        self.X = X
+        return X @ self.W + self.b
+
+    def backward(self, dZ):
+        self.grad_W = self.X.T @ dZ
+        self.grad_b = np.sum(dZ, axis=0, keepdims=True)
+        dX = dZ @ self.W.T
+        return dX 
